95a0205136aafd8b2274e00b983037e0d6791e29
Ahmed <ahmedazmycs@protonmail.com>
2022-05-20 13:41:20 +0200

    fixed for arabic reesl section

--- content-script.js
+++ content-script.js
@@ -1,18 +1,17 @@
 //wait for DOM element to render
 const waitForReels = () => {
   console.log("remove reels extension is watching content");
-  const selector = "[aria-label=Reels]";
-  if (document.querySelector(selector)) {
-    document.querySelector(selector).parentNode.parentNode.parentNode.remove();
-    console.log("reels removed");
-  }
+  const selectors = ["[aria-label=Reels]", "[aria-label=ريلز]"];
+
   const observer = new MutationObserver(() => {
-    if (document.querySelector(selector)) {
-      document
-        .querySelector(selector)
-        .parentNode.parentNode.parentNode.remove();
-      console.log("reels removed");
-    }
+    selectors.forEach((selector) => {
+      if (document.querySelector(selector)) {
+        document
+          .querySelector(selector)
+          .parentNode.parentNode.parentNode.remove();
+        console.log("reels removed");
+      }
+    });
   });
   observer.observe(document.body, {
     childList: true,
